2f8409b9d698365ae730a16452d1c4706db601cb
jacob <jalbert90@gmail.com>
2022-09-28 09:55:06 -0400

    Potentially solved

--- main.cpp
+++ main.cpp
@@ -8,14 +8,22 @@ int main() {
 	const int NUMBER_OF_FRIENDS = 3;
 	int whoHasSolutions[MAX_NUMBER_OF_PROBLEMS][3];
 	int numberOfProblems;
+	int numberOfProblemsToBeSolved = 0;
 
 	cin >> numberOfProblems;
 
 	for (int i = 0; i < numberOfProblems; i++) {
+		int numberOfFriendsWithSolutions = 0;
 		for (int j = 0; j < NUMBER_OF_FRIENDS; j++) {
 			cin >> whoHasSolutions[i][j];
+			if (whoHasSolutions[i][j] == 1) {
+				numberOfFriendsWithSolutions++;
+			}
+		}
+		if (numberOfFriendsWithSolutions >= 2) {
+			numberOfProblemsToBeSolved++;
 		}
 	}
-	cout << whoHasSolutions[0][2];
+	cout << numberOfProblemsToBeSolved;
 	return 0;
 }
